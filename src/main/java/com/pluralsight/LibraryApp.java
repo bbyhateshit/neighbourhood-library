16fc0c9ee6be527a4689f84c3a65a685d5b55d1a
bbyhateshit <j.galeta@gmail.com>
2026-04-17 13:47:05 -0400

    Added my books to the main.

--- src/main/java/com/pluralsight/LibraryApp.java
+++ src/main/java/com/pluralsight/LibraryApp.java
@@ -3,5 +3,31 @@ package com.pluralsight;
 public class LibraryApp {
     public static void main(String[] args) {
 
+        Book[] books = new Book[] {
+                new Book(1,  "978-0143127-741", "To Kill a Mockingbird", "", false),
+                new Book(2,  "978-0307277-671", "1984", "", false),
+                new Book(3,  "978-0061120-084", "Brave New World", "", false),
+                new Book(4,  "978-0743273-565", "The Great Gatsby", "", false),
+                new Book(5,  "978-0307387-899", "Fahrenheit 451", "", false),
+                new Book(6,  "978-0439023-528", "The Hunger Games", "", false),
+                new Book(7,  "978-0544003-415", "The Hobbit", "", false),
+                new Book(8,  "978-0307474-278", "The Road", "", false),
+                new Book(9,  "978-0060850-524", "The Alchemist", "", false),
+                new Book(10, "978-0385472-579", "The Things They Carried", "", false),
+                new Book(11, "978-0140283-334", "Lord of the Flies", "", false),
+                new Book(12, "978-0307346-605", "The Book Thief", "", false),
+                new Book(13, "978-0062315-007", "The Giver", "", false),
+                new Book(14, "978-0143039-433", "Animal Farm", "", false),
+                new Book(15, "978-0307278-449", "Life of Pi", "", false),
+                new Book(16, "978-0060935-467", "Beloved", "", false),
+                new Book(17, "978-0307389-732", "The Kite Runner", "", false),
+                new Book(18, "978-0142437-230", "Moby-Dick", "", false),
+                new Book(19, "978-0140449-266", "Crime and Punishment", "", false),
+                new Book(20, "978-0140449-181", "The Odyssey", "", false)
+        };
+
+
+
     }
+
 }
